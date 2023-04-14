803b566a897e2dbb2e9808c80dd019f983981b18
yulingxinluo <57789575+yulingxinluo@users.noreply.github.com>
2023-04-14 13:31:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,15 +1,8 @@
 /* eslint-disable */
-
-/**
- * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
- * 自行添加属性，以支持更多个性化功能
- */
 const USER_CONFIG = {
 USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx3b68feab4143d7cd',
-
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '957a0db13256173297d466ac04882469',
 
@@ -21,7 +14,7 @@ USE_PASSAGE: 'push-deer',
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU21885TdyinkhyRAoLAMpZmbb2cfTL9k3bK6qRy',
+      id: '3bb21884eb9343c698f080957f06689f',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
