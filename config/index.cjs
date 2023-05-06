52673577a5346980f603642d77d0dc1c908ae8fb
yulingxinluo <57789575+yulingxinluo@users.noreply.github.com>
2023-05-06 14:06:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -17,7 +17,7 @@ const USER_CONFIG = {
     {
       name: '阿鱼',
       id: 'oZ4U66_o4IzfKsRzARPkX8Lv2sBo',
-      useTemplateId:'Nm53xTb5oq3lRDeAqMjx25U5JBFAOzs9A_xK2Fagpa0',
+      useTemplateId:'cDtQM_iiNtwUPn1NRtFRD_FqRLuXhODlTzhBa-h5RiY',
       province: '浙江',
       city: '杭州',
       festivals: [
